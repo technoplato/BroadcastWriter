f8a1ccf89522651139aa254f45d122eda8f966cf
Michael Lustig <lustig@knophy.com>
2023-08-10 09:51:26 -0400

    Add SwiftUI target

---
+++ ExampleSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExampleSwiftUI
+//
+//  Created by laptop on 8/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Goodbye, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExampleSwiftUI/ExampleSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleSwiftUIApp.swift
+//  ExampleSwiftUI
+//
+//  Created by laptop on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ExampleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
