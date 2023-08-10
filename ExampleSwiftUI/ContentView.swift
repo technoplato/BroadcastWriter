a7504b72e98953a9dd9dc7cd8f0fe8922c193d3b
Michael Lustig <lustig@knophy.com>
2023-08-10 09:56:20 -0400

    Display shared app group contents in SwiftUI

--- ExampleSwiftUI/ContentView.swift
+++ ExampleSwiftUI/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Goodbye, world!")
+          SharedFilesView(viewModel: SharedFilesViewModel())
         }
         .padding()
     }
@@ -21,4 +22,52 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
+}
+
+
+class SharedFilesViewModel: ObservableObject {
+    @Published var files: [String] = []
+    
+    private let fileManager = FileManager.default
+    private let containerURL: URL?
+    
+    init() {
+        containerURL = fileManager.containerURL(forSecurityApplicationGroupIdentifier: "group.com.lustig.Example")
+    }
+    
+    func fetchFiles() {
+        guard let containerURL = containerURL else {
+            debugPrint("no container directory")
+            return
+        }
+
+        do {
+            let contents = try fileManager.contentsOfDirectory(atPath: containerURL.path)
+            self.files = contents
+            for path in contents {
+                let fullPath = containerURL.appendingPathComponent(path).path
+                debugPrint("Full path of content in shared container:", fullPath)
+            }
+        } catch {
+            debugPrint("Error reading contents:", error)
+        }
+    }
+}
+
+struct SharedFilesView: View {
+    @ObservedObject var viewModel: SharedFilesViewModel
+    
+    var body: some View {
+        VStack {
+            Button("Refresh") {
+                viewModel.fetchFiles()
+            }
+            List(viewModel.files, id: \.self) { file in
+                Text(file)
+            }
+        }
+        .onAppear {
+            viewModel.fetchFiles()
+        }
+    }
 }
